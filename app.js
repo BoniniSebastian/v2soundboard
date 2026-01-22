7489ce3ebd4498568d0ab8a887f548b54656e74c
BoniniSebastian <sebastian.bonini@gmail.com>
2026-01-22 15:34:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const REPO  = "v2soundboard";
 
 const CATEGORIES = [
   { title: "Tuta", folder: "sounds/tuta" },
-  { title: "Mål", folder: "sounds/mal" },
+  { title: "Goal", folder: "sounds/mal" },
   { title: "Utvisning", folder: "sounds/utvisning" }
 ];
 
